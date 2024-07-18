68787aea24ab1033ed8529c1456a399e458e5f75
cardosofede <federico.cardoso.e@gmail.com>
2024-07-18 16:43:42 +0300

    (feat) add caching for candles

--- data_sources/clob.py
+++ data_sources/clob.py
@@ -1,5 +1,7 @@
 import time
+from typing import Dict, Tuple
 
+import pandas as pd
 from hummingbot.client.config.client_config_map import ClientConfigMap
 from hummingbot.client.config.config_helpers import ClientConfigAdapter, get_connector_class
 from hummingbot.client.settings import AllConnectorSettings, ConnectorType
@@ -22,6 +24,7 @@ class ClobDataSource:
         self.connectors = {name: self.get_connector(name) for name, settings in self.conn_settings.items()
                            if settings.type in self.CONNECTOR_TYPES and name not in self.EXCLUDED_CONNECTORS and
                            "testnet" not in name}
+        self.candles_cache: Dict[Tuple[str, str, str], pd.DataFrame] = {}
 
     @staticmethod
     def get_connector_config_map(connector_name: str):
@@ -33,8 +36,28 @@ class ClobDataSource:
                           trading_pair: str,
                           interval: str,
                           start_time: int,
-                          end_time: int):
-        # TODO: Add cache based on start and end time
+                          end_time: int) -> OHLC:
+        cache_key = (connector_name, trading_pair, interval)
+
+        if cache_key in self.candles_cache:
+            cached_df = self.candles_cache[cache_key]
+            cached_start_time = cached_df.index.min().timestamp()
+            cached_end_time = cached_df.index.max().timestamp()
+
+            if cached_start_time <= start_time and cached_end_time >= end_time:
+                return OHLC(cached_df[(cached_df.index >= pd.to_datetime(start_time, unit='s')) &
+                                      (cached_df.index <= pd.to_datetime(end_time, unit='s'))])
+            else:
+                if start_time < cached_start_time:
+                    new_start_time = start_time
+                    new_end_time = cached_start_time - 1
+                else:
+                    new_start_time = cached_end_time + 1
+                    new_end_time = end_time
+        else:
+            new_start_time = start_time
+            new_end_time = end_time
+
         candle = self.candles_factory.get_candle(CandlesConfig(
             connector=connector_name,
             trading_pair=trading_pair,
@@ -43,17 +66,27 @@ class ClobDataSource:
         candles_df = await candle.get_historical_candles(HistoricalCandlesConfig(
             connector_name=connector_name,
             trading_pair=trading_pair,
-            start_time=start_time,
-            end_time=end_time,
+            start_time=new_start_time,
+            end_time=new_end_time,
             interval=interval
         ))
-        return OHLC(candles_df)
+        candles_df.index = pd.to_datetime(candles_df.timestamp, unit='s')
+
+        if cache_key in self.candles_cache:
+            self.candles_cache[cache_key] = pd.concat(
+                [self.candles_cache[cache_key], candles_df]).drop_duplicates().sort_index()
+        else:
+            self.candles_cache[cache_key] = candles_df
+
+        return OHLC(self.candles_cache[cache_key][
+                        (self.candles_cache[cache_key].index >= pd.to_datetime(start_time, unit='s')) &
+                        (self.candles_cache[cache_key].index <= pd.to_datetime(end_time, unit='s'))])
 
     async def get_candles_last_days(self,
                                     connector_name: str,
                                     trading_pair: str,
                                     interval: str,
-                                    days: int):
+                                    days: int) -> OHLC:
         end_time = int(time.time())
         start_time = end_time - days * 24 * 60 * 60
         return await self.get_candles(connector_name, trading_pair, interval, start_time, end_time)
