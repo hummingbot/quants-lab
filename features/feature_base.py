2b2e8d599949a64e077e43150516f8fcf5ecf456
cardosofede <federico.cardoso.e@gmail.com>
2024-07-18 16:43:24 +0300

    (feat) add feature config

--- features/feature_base.py
+++ features/feature_base.py
@@ -1,15 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel
+
+# Define a type variable that can be any subclass of FeatureConfig
+T = TypeVar('T', bound='FeatureConfig')
 
 
-class FeatureBase(ABC):
-    def __init__(self, **kwargs):
-        self.params = kwargs
+class FeatureConfig(BaseModel):
+    name: str
+
+
+class FeatureBase(ABC, Generic[T]):
+    def __init__(self, feature_config: T):
+        self.config = feature_config
 
     @abstractmethod
     def calculate(self, data):
         """
-        This method should be implemented by the child class to calculate the feature and add it to the data_handler.
-        :param data_handler:
+        This method should be implemented by the child class to calculate the feature and add it to the data.
+        :param data:
         :return:
         """
         ...
