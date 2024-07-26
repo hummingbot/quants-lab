04518e8374af8cd9c58003c3ef0f6626ef52fee9
cardosofede <federico.cardoso.e@gmail.com>
2024-07-26 08:18:19 +0300

    (feat) normalize natr

--- features/candles/volatility.py
+++ features/candles/volatility.py
@@ -12,7 +12,7 @@ class Volatility(FeatureBase[VolatilityConfig]):
     def calculate(self, candles):
         window = self.config.window
         candles["volatility"] = candles["close"].pct_change().rolling(window=window).std()
-        candles["natr"] = ta.natr(candles["high"], candles["low"], candles["close"], length=window)
+        candles["natr"] = ta.natr(candles["high"], candles["low"], candles["close"], length=window) / 100
         bbands = ta.bbands(candles["close"], length=window)
         candles["bb_width"] = bbands[f"BBB_{window}_2.0"]
         return candles
