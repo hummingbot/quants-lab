d0f2d1cbfcd0ef7147f8139b125b23d2fa3625b4
cardosofede <federico.cardoso.e@gmail.com>
2024-08-17 12:33:35 -0300

    (feat) re-organize features

--- features/candles/trend.py
+++ features/candles/trend.py
@@ -23,11 +23,13 @@ class Trend(FeatureBase[TrendConfig]):
 
         # Calculate the trend score for each row
         candles = candles.copy()
-        candles['trend_score'] = self.calculate_trending_score(candles['close'], short_window, long_window)
-
+        trend_score, short_slope, long_slope = self.calculate_trending_score(candles['close'], short_window, long_window)
+        candles['trend_score'] = trend_score
+        candles['short_slope'] = short_slope
+        candles['long_slope'] = long_slope
         return candles
 
-    def calculate_trending_score(self, series: pd.Series, short_window: int, long_window: int) -> pd.Series:
+    def calculate_trending_score(self, series: pd.Series, short_window: int, long_window: int):
         short_mavg = series.rolling(window=short_window, min_periods=1).mean()
         long_mavg = series.rolling(window=long_window, min_periods=1).mean()
 
@@ -35,7 +37,7 @@ class Trend(FeatureBase[TrendConfig]):
         long_slope = long_mavg.rolling(window=long_window, min_periods=1).apply(self.calculate_slope, raw=True)
 
         trending_score = (short_slope - long_slope) / (np.abs(short_slope) + np.abs(long_slope))
-        return trending_score
+        return trending_score, short_slope, long_slope
 
     @staticmethod
     def calculate_slope(values: np.ndarray) -> float:
