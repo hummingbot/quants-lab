6ea67fc5cf1bcbfc1fc855cf738b5bd6fdc6c135
cardosofede <federico.cardoso.e@gmail.com>
2024-09-30 20:57:43 -0300

    (feat) add from trade id

--- core/data_sources/trades_feed/trades_feed_base.py
+++ core/data_sources/trades_feed/trades_feed_base.py
@@ -13,12 +13,13 @@ class TradesFeedBase(ABC):
     def get_exchange_trading_pair(self, trading_pair: str) -> str:
         ...
 
-    async def get_historical_trades(self, trading_pair: str, start_time: int, end_time: Optional[int] = None):
+    async def get_historical_trades(self, trading_pair: str, start_time: int, end_time: Optional[int] = None,
+                                    from_id: Optional[int] = None):
         if not end_time:
-            end_time = time.time()
-        historical_trades = await self._get_historical_trades(trading_pair, start_time, end_time)
+            end_time = int(time.time())
+        historical_trades = await self._get_historical_trades(trading_pair, start_time, end_time, from_id)
         return historical_trades
 
     @abstractmethod
-    async def _get_historical_trades(self, trading_pair: str, start_time: int, end_time: int):
+    async def _get_historical_trades(self, trading_pair: str, start_time: int, end_time: int, from_id: Optional[int] = None):
         ...
