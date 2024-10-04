9d95c629ef4d921386b6c0b5edf1639bf25569e6
cardosofede <federico.cardoso.e@gmail.com>
2024-10-04 19:09:01 -0300

    (feat) update tasks

--- tasks/data_collection/trades_downloader_task.py
+++ tasks/data_collection/trades_downloader_task.py
@@ -19,16 +19,19 @@ class TradesDownloaderTask(BaseTask):
     def __init__(self, name: str, frequency: timedelta, config: Dict[str, Any]):
         super().__init__(name, frequency, config)
         self.connector_name = config['connector_name']
+        self.days_data_retention = config.get("days_data_retention", 7)
+        self.start_time = time.time() - self.days_data_retention * 24 * 60 * 60
         self.quote_asset = config.get('quote_asset', "USDT")
         self.min_notional_size = Decimal(str(config.get('min_notional_size', 10.0)))
         self.clob = CLOBDataSource()
 
     async def execute(self):
-        logging.info(f"Starting trades downloader for {self.connector_name} at {time.strftime('%Y-%m-%d %H:%M:%S')}")
+        now = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f UTC')
+        logging.info(f"{now} - Starting trades downloader for {self.connector_name} at {time.strftime('%Y-%m-%d %H:%M:%S')}")
         end_time = datetime.now(timezone.utc)
-        start_time = pd.Timestamp(self.config["start_time"]).tz_localize(timezone.utc)
-        logging.info(f"Start date: {start_time}, End date: {end_time}")
-        logging.info(f"Quote asset: {self.quote_asset}, Min notional size: {self.min_notional_size}")
+        start_time = pd.Timestamp(self.start_time, unit="s").tz_localize(timezone.utc)
+        logging.info(f"{now} - Start date: {start_time}, End date: {end_time}")
+        logging.info(f"{now} - Quote asset: {self.quote_asset}, Min notional size: {self.min_notional_size}")
 
         timescale_client = TimescaleClient(
             host=os.getenv("POSTGRES_HOST", "localhost"),
@@ -38,17 +41,18 @@ class TradesDownloaderTask(BaseTask):
             database=os.getenv("POSTGRES_DB", "timescaledb")
         )
         await timescale_client.connect()
-        await timescale_client.create_trades_table()
 
         trading_rules = await self.clob.get_trading_rules(self.connector_name)
         trading_pairs = trading_rules.filter_by_quote_asset(self.quote_asset) \
             .filter_by_min_notional_size(self.min_notional_size) \
             .get_all_trading_pairs()
-
         for i, trading_pair in enumerate(trading_pairs):
-            logging.info(f"Fetching trades for {trading_pair} [{i} from {len(trading_pairs)}]")
+            logging.info(f"{now} - Fetching trades for {trading_pair} [{i} from {len(trading_pairs)}]")
             try:
-                last_trade_id = await timescale_client.get_last_trade_id(self.connector_name, trading_pair)
+                table_name = timescale_client.get_trades_table_name(self.connector_name, trading_pair)
+                last_trade_id = await timescale_client.get_last_trade_id(connector_name=self.connector_name,
+                                                                         trading_pair=trading_pair,
+                                                                         table_name=table_name)
                 trades = await self.clob.get_trades(
                     self.connector_name,
                     trading_pair,
@@ -58,7 +62,7 @@ class TradesDownloaderTask(BaseTask):
                 )
 
                 if trades.empty:
-                    logging.info(f"No new trades for {trading_pair}")
+                    logging.info(f"{now} - No new trades for {trading_pair}")
                     continue
 
                 trades["connector_name"] = self.connector_name
@@ -68,11 +72,18 @@ class TradesDownloaderTask(BaseTask):
                     ["id", "connector_name", "trading_pair", "timestamp", "price", "volume",
                      "sell_taker"]].values.tolist()
 
-                await timescale_client.append_trades(trades_data)
-                logging.info(f"Inserted {len(trades_data)} trades for {trading_pair}")
+                await timescale_client.append_trades(table_name=table_name,
+                                                     trades=trades_data)
+                today_start = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+                cutoff_timestamp = (today_start - timedelta(days=self.days_data_retention)).timestamp()
+                await timescale_client.delete_trades(connector_name=self.connector_name, trading_pair=trading_pair,
+                                                     timestamp=cutoff_timestamp)
+                await timescale_client.compute_resampled_ohlc(connector_name=self.connector_name,
+                                                              trading_pair=trading_pair, interval="1s")
+                logging.info(f"{now} - Inserted {len(trades_data)} trades for {trading_pair}")
 
             except Exception as e:
-                logging.exception(f"An error occurred during the data load for trading pair {trading_pair}:\n {e}")
+                logging.exception(f"{now} - An error occurred during the data load for trading pair {trading_pair}:\n {e}")
                 continue
 
         await timescale_client.close()
@@ -87,5 +98,6 @@ if __name__ == "__main__":
         'db_port': 5432,
         'db_name': 'timescaledb'
     }
+
     task = TradesDownloaderTask("Trades Downloader", timedelta(hours=1), config)
     asyncio.run(task.execute())
--- tasks/task_runner.py
+++ tasks/task_runner.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import os
 import sys
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
@@ -13,30 +13,31 @@ logger = logging.getLogger(__name__)
 
 async def main():
     from core.task_base import TaskOrchestrator
-    from core.utils import load_dict_from_yaml
-    from tasks.backtesting.xtreet_backtesting_task import BacktestingTask
+
+    # from core.utils import load_dict_from_yaml
+    # from tasks.backtesting.xtreet_backtesting_task import BacktestingTask
     from tasks.data_collection.trades_downloader_task import TradesDownloaderTask
     orchestrator = TaskOrchestrator()
 
-    backtesting_config = {
-        'lookback_days': 1,
-        'config': load_dict_from_yaml(file_name="binance_config.yml", folder=f"{project_root}/tasks/config"),
-        'resolution': "1s",
-        'study_name': "xtreet_1",
-        'root_path': project_root
-    }
-    backtesting_task = BacktestingTask("Backtesting", timedelta(hours=24), backtesting_config)
+    # backtesting_config = {
+    #     'lookback_days': 1,
+    #     'config': load_dict_from_yaml(file_name="binance_config.yml", folder=f"{project_root}/tasks/config"),
+    #     'resolution': "1s",
+    #     'study_name': "xtreet_1",
+    #     'root_path': project_root
+    # }
+    # backtesting_task = BacktestingTask("Backtesting", timedelta(hours=24), backtesting_config)
 
     trades_downloader_config = {
         'connector_name': 'binance_perpetual',
         'quote_asset': 'USDT',
         'min_notional_size': 10.0,
-        'start_time': datetime(2024, 9, 23),
+        'days_data_retention': 3
     }
     trades_downloader_task = TradesDownloaderTask("Trades Downloader Binance", timedelta(hours=5),
                                                   trades_downloader_config)
 
-    orchestrator.add_task(backtesting_task)
+    # orchestrator.add_task(backtesting_task)
     orchestrator.add_task(trades_downloader_task)
 
     await orchestrator.run()
