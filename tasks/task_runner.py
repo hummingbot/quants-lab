485fce4ee432b87cc04f273cdf448ebd73032e53
cardosofede <federico.cardoso.e@gmail.com>
2024-11-06 00:14:19 -0300

    (feat) update tasks

--- tasks/task_runner.py
+++ tasks/task_runner.py
@@ -13,19 +13,28 @@ logger = logging.getLogger(__name__)
 
 async def main():
     from core.task_base import TaskOrchestrator
+    from core.utils import load_dict_from_yaml
+    from tasks.backtesting.xtreet_backtesting_task import BacktestingTask
     from tasks.data_collection.trades_downloader_task import TradesDownloaderTask
     orchestrator = TaskOrchestrator()
 
-    trades_downloader_task = TradesDownloaderTask(
-        name="Trades Downloader Binance",
-        frequency=timedelta(hours=5),
-        config={
-            'connector_name': 'binance_perpetual',
-            'quote_asset': 'USDT',
-            'min_notional_size': 10.0,
-            'days_data_retention': 3
-        })
+    backtesting_config = {
+        'config': load_dict_from_yaml(file_name="binance_config.yml", folder=f"{project_root}/tasks/config"),
+        'resolution': "1s",
+        'root_path': project_root
+    }
+    backtesting_task = BacktestingTask("Backtesting", timedelta(hours=12), backtesting_config)
 
+    trades_downloader_config = {
+        'connector_name': 'binance_perpetual',
+        'quote_asset': 'USDT',
+        'min_notional_size': 10.0,
+        'days_data_retention': 3
+    }
+    trades_downloader_task = TradesDownloaderTask("Trades Downloader Binance", timedelta(hours=5),
+                                                  trades_downloader_config)
+
+    orchestrator.add_task(backtesting_task)
     orchestrator.add_task(trades_downloader_task)
 
     await orchestrator.run()
