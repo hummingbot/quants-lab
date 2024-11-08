269f84c1b1ff0746d6519d153d4fb9299b57c235
drupman <tomasgaudino@hotmail.com>
2024-11-08 01:24:47 -0300

    (feat) split task and runner in db status

--- tasks/db_status_runner.py
+++ tasks/db_status_runner.py
@@ -1,52 +1,33 @@
 import asyncio
 import logging
-import time
 import os
 import sys
-from datetime import datetime, timedelta, timezone
-from typing import Any, Dict
+from datetime import timedelta
+from dotenv import load_dotenv
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
-
-from core.services.timescale_client import TimescaleClient
-from core.task_base import BaseTask
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-class DbStatusTask(BaseTask):
-    def __init__(self, name: str, frequency: timedelta, config: Dict[str, Any]):
-        super().__init__(name, frequency, config)
+async def main():
+    from core.task_base import TaskOrchestrator
+    from tasks.data_collection.db_status_task import DbStatusTask
+    orchestrator = TaskOrchestrator()
 
-    async def execute(self):
-        now = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f UTC')
-        logging.info(f"{now} - Generating metrics report for TimescaleDB at {time.strftime('%Y-%m-%d %H:%M:%S')}")
-
-        timescale_client = TimescaleClient(
-            host=self.config["db_host"],
-            port=self.config["db_port"],
-        )
-        await timescale_client.connect()
-        available_trading_pairs = await timescale_client.get_available_pairs()
-        for i, market_info in enumerate(available_trading_pairs):
-            connector_name, trading_pair = market_info
-            logging.info(f"{now} - Fetching trades for {trading_pair} [{i} from {len(available_trading_pairs)}]")
-            try:
-                logging.info(f"{now} - Updated metrics for {trading_pair}")
-                await timescale_client.append_metrics(connector_name=connector_name, trading_pair=trading_pair)
-            except Exception as e:
-                logging.exception(f"{now} - An error occurred during the data load for trading pair {trading_pair}:\n {e}")
-                continue
-
-        await timescale_client.close()
-
-
-if __name__ == "__main__":
     config = {
-        'db_host': 'localhost',
+        'db_host': os.getenv("TIMESCALE_HOST", 'localhost'),
         'db_port': 5432,
     }
 
-    task = DbStatusTask("Metrics Report", timedelta(hours=1), config)
-    asyncio.run(task.execute())
+    db_status_task = DbStatusTask("Metrics Report", timedelta(hours=1), config)
+    orchestrator.add_task(db_status_task)
+
+    await orchestrator.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
