224a22e3c04bc1f4b86e6459c20643608aeacc6e
drupman <tomasgaudino@hotmail.com>
2024-11-09 16:16:45 -0300

    (feat) add now method cause it was freezed

--- tasks/data_collection/trades_downloader_task.py
+++ tasks/data_collection/trades_downloader_task.py
@@ -27,12 +27,11 @@ class TradesDownloaderTask(BaseTask):
         self.clob = CLOBDataSource()
 
     async def execute(self):
-        now = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f UTC')
-        logging.info(f"{now} - Starting trades downloader for {self.connector_name} at {time.strftime('%Y-%m-%d %H:%M:%S')}")
+        logging.info(f"{self.now()} - Starting trades downloader for {self.connector_name} at {time.strftime('%Y-%m-%d %H:%M:%S')}")
         end_time = datetime.now(timezone.utc)
         start_time = pd.Timestamp(self.start_time, unit="s").tz_localize(timezone.utc)
-        logging.info(f"{now} - Start date: {start_time}, End date: {end_time}")
-        logging.info(f"{now} - Quote asset: {self.quote_asset}, Min notional size: {self.min_notional_size}")
+        logging.info(f"{self.now()} - Start date: {start_time}, End date: {end_time}")
+        logging.info(f"{self.now()} - Quote asset: {self.quote_asset}, Min notional size: {self.min_notional_size}")
 
         timescale_client = TimescaleClient(
             host=self.config["timescale_config"]["host"],
@@ -48,7 +47,7 @@ class TradesDownloaderTask(BaseTask):
             .filter_by_min_notional_size(self.min_notional_size) \
             .get_all_trading_pairs()
         for i, trading_pair in enumerate(trading_pairs):
-            logging.info(f"{now} - Fetching trades for {trading_pair} [{i} from {len(trading_pairs)}]")
+            logging.info(f"{self.now()} - Fetching trades for {trading_pair} [{i} from {len(trading_pairs)}]")
             try:
                 table_name = timescale_client.get_trades_table_name(self.connector_name, trading_pair)
                 last_trade_id = await timescale_client.get_last_trade_id(connector_name=self.connector_name,
@@ -63,7 +62,7 @@ class TradesDownloaderTask(BaseTask):
                 )
 
                 if trades.empty:
-                    logging.info(f"{now} - No new trades for {trading_pair}")
+                    logging.info(f"{self.now()} - No new trades for {trading_pair}")
                     continue
 
                 trades["connector_name"] = self.connector_name
@@ -84,13 +83,17 @@ class TradesDownloaderTask(BaseTask):
                 await timescale_client.compute_resampled_ohlc(connector_name=self.connector_name,
                                                               trading_pair=trading_pair, interval="1s")
 
-                logging.info(f"{now} - Inserted {len(trades_data)} trades for {trading_pair}")
+                logging.info(f"{self.now()} - Inserted {len(trades_data)} trades for {trading_pair}")
 
             except Exception as e:
-                logging.exception(f"{now} - An error occurred during the data load for trading pair {trading_pair}:\n {e}")
+                logging.exception(f"{self.now()} - An error occurred during the data load for trading pair {trading_pair}:\n {e}")
                 continue
 
         await timescale_client.close()
+
+    @staticmethod
+    def now():
+        return datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f UTC')
 
 
 if __name__ == "__main__":
