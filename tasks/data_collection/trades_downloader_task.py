f8eeb614de04de1ccf6fffe2e952605b5aeff995
drupman <tomasgaudino@hotmail.com>
2024-11-18 20:14:55 -0300

    (feat) update trades_downloader_task.py to accept just selected pairs

--- tasks/data_collection/trades_downloader_task.py
+++ tasks/data_collection/trades_downloader_task.py
@@ -23,6 +23,7 @@ class TradesDownloaderTask(BaseTask):
         self.days_data_retention = config.get("days_data_retention", 7)
         self.start_time = time.time() - self.days_data_retention * 24 * 60 * 60
         self.quote_asset = config.get('quote_asset', "USDT")
+        self.selected_pairs = config.get('selected_pairs')
         self.min_notional_size = Decimal(str(config.get('min_notional_size', 10.0)))
         self.clob = CLOBDataSource()
 
@@ -46,6 +47,9 @@ class TradesDownloaderTask(BaseTask):
         trading_pairs = trading_rules.filter_by_quote_asset(self.quote_asset) \
             .filter_by_min_notional_size(self.min_notional_size) \
             .get_all_trading_pairs()
+        if self.selected_pairs is not None:
+            trading_pairs = [sorted([trading_pair for trading_pair in trading_pairs
+                                     if trading_pair in self.selected_pairs])]
         for i, trading_pair in enumerate(trading_pairs):
             logging.info(f"{self.now()} - Fetching trades for {trading_pair} [{i} from {len(trading_pairs)}]")
             try:
@@ -78,8 +82,6 @@ class TradesDownloaderTask(BaseTask):
                 cutoff_timestamp = (today_start - timedelta(days=self.days_data_retention)).timestamp()
                 await timescale_client.delete_trades(connector_name=self.connector_name, trading_pair=trading_pair,
                                                      timestamp=cutoff_timestamp)
-                # TODO: isolate resampling and metrics management in another module
-                # TODO: pass list of intervals to perform better
                 await timescale_client.compute_resampled_ohlc(connector_name=self.connector_name,
                                                               trading_pair=trading_pair, interval="1s")
 
@@ -103,7 +105,8 @@ if __name__ == "__main__":
         'min_notional_size': 10.0,
         'db_host': 'localhost',
         'db_port': 5432,
-        'db_name': 'timescaledb'
+        'db_name': 'timescaledb',
+        'selected_pairs': ["BTC-USDT", "ETH-USDT"]
     }
 
     task = TradesDownloaderTask("Trades Downloader", timedelta(hours=1), config)
