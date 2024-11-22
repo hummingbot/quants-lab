89b376eeb50c9f56dca967ada32536bc93896440
cardosofede <federico.cardoso.e@gmail.com>
2024-11-22 19:20:00 -0300

    (feat) add local cache update task

--- tasks/data_collection/local_cache_update_task.py
+++ tasks/data_collection/local_cache_update_task.py
@@ -15,58 +15,56 @@ logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
 
-
 class LocalCacheExportTask(BaseTask):
     def __init__(self, name: str, frequency: timedelta, config: Dict[str, Any]):
         super().__init__(name, frequency, config)
-        self.connector_name = config["connector_name"]
         self.output_dir = Path(os.path.join(config.get("root_path", ""), config.get("output_dir", "data/candles")))
         self.output_dir.mkdir(parents=True, exist_ok=True)
 
     async def execute(self):
-        now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f UTC")
-        logging.info(f"{now} - Starting candles export for {self.connector_name}")
+        logging.info(f"Starting candles export for {self.config['connector_name']}")
 
         timescale_client = TimescaleClient(
-            host=self.config["timescale_config"].get("db_host", "localhost"),
-            port=self.config["timescale_config"].get("db_port", 5432),
-            user=self.config["timescale_config"].get("db_user", "admin"),
-            password=self.config["timescale_config"].get("db_password", "admin"),
-            database=self.config["timescale_config"].get("db_name", "timescaledb")
+            host=self.config["timescale_config"].get("host", "localhost"),
+            port=self.config["timescale_config"].get("port", 5432),
+            user=self.config["timescale_config"].get("user", "admin"),
+            password=self.config["timescale_config"].get("password", "admin"),
+            database=self.config["timescale_config"].get("database", "timescaledb")
         )
         await timescale_client.connect()
 
         available_candles = await timescale_client.get_available_candles()
         for connector_name, trading_pair, interval in available_candles:
-            if connector_name != self.connector_name:
+            if connector_name != self.config["connector_name"] or trading_pair not in self.config["selected_pairs"]:
                 continue
-
+            now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f UTC")
             logging.info(f"{now} - Exporting candles for {trading_pair} - {interval}")
             try:
                 # Get all candles from the database
                 candles = await timescale_client.get_all_candles(
-                    connector_name=self.connector_name,
-                        trading_pair=trading_pair,
-                        interval=interval
-                    )
+                    connector_name=self.config["connector_name"],
+                    trading_pair=trading_pair,
+                    interval=interval
+                )
                 candles_df: pd.DataFrame = candles.data
                 candles_df["timestamp"] = candles_df["timestamp"].apply(lambda x: x.timestamp())
+                candles_df.sort_values("timestamp", inplace=True)
                 if candles_df.empty:
                     logging.info(f"{now} - No data found for {trading_pair} - {interval}")
                     continue
-                filename = f"{self.connector_name}|{trading_pair}|{interval}.parquet"
+                filename = f"{self.config['connector_name']}|{trading_pair}|{interval}.parquet"
 
                 # Save to parquet with trading pair and interval in filename
                 filepath = self.output_dir / filename
 
                 # Save with compression
                 candles_df.to_parquet(
-                        filepath,
-                        engine='pyarrow',
-                        compression='snappy',
-                        index=True
-                    )
-                    
+                    filepath,
+                    engine='pyarrow',
+                    compression='snappy',
+                    index=True
+                )
+
                 logging.info(f"{now} - Saved {len(candles_df)} candles to {filepath}")
 
             except Exception as e:
@@ -84,20 +82,23 @@ async def main(config):
     )
     await candles_export_task.execute()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     timescale_config = {
-        "host": os.getenv("TIMESCALE_HOST", "localhost"),
-        "port": os.getenv("TIMESCALE_PORT", 5432),
-        "user": os.getenv("TIMESCALE_USER", "admin"),
-        "password": os.getenv("TIMESCALE_PASSWORD", "admin"),
-        "database": os.getenv("TIMESCALE_DB", "timescaledb")
+        "host": "63.250.52.93",
+        "port": 5432,
+        "user": "admin",
+        "password": "admin",
+        "database": "timescaledb"
     }
     config = {
         "root_path": os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')),
         "connector_name": "binance_perpetual",
         "output_dir": "data/candles",  # Use root path here
-        "timescale_config": timescale_config
+        "timescale_config": timescale_config,
+        "selected_pairs": ['1000SHIB-USDT', 'WLD-USDT', 'ACT-USDT', '1000BONK-USDT', 'DOGE-USDT', 'AGLD-USDT',
+                           'SUI-USDT', '1000SATS-USDT', 'MOODENG-USDT', 'NEIRO-USDT', 'HBAR-USDT', 'ENA-USDT',
+                           'HMSTR-USDT', 'TROY-USDT', '1000PEPE-USDT', '1000X-USDT', 'PNUT-USDT', 'SOL-USDT',
+                           'XRP-USDT', 'SWELL-USDT'],
     }
     asyncio.run(main(config))
-
