21efca0e1677ee40674b8df95a3470f67a6da49b
madpower2000 <madpower2000@gmail.com>
2025-07-29 16:31:32 +0300

    ppm_simple basic setup

--- nexus/notebooks/local_pmm_simple_backtest.py
+++ nexus/notebooks/local_pmm_simple_backtest.py
@@ -4,6 +4,13 @@ import os
 import sys
 from decimal import Decimal
 
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+)
+
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))  # noqa: E402
 sys.path.append(root_path)  # noqa: E402
 
@@ -27,7 +34,9 @@ config = PMMSimpleConfig(
     total_amount_quote=Decimal("1000"),
     take_profit=Decimal("0.003"),
     stop_loss=Decimal("0.003"),
-    trailing_stop=TrailingStop(activation_price=Decimal("0.001"), trailing_delta=Decimal("0.0005")),
+    trailing_stop=TrailingStop(
+        activation_price=Decimal("0.001"), trailing_delta=Decimal("0.0005")
+    ),
     time_limit=60 * 60,
     cooldown_time=60 * 60,
     executor_refresh_time=60,
@@ -36,7 +45,6 @@ config = PMMSimpleConfig(
 start = int(datetime.datetime(2024, 1, 1).timestamp())
 end = int(datetime.datetime(2024, 1, 2).timestamp())
 
-
 async def main():
     clob = CLOBDataSource(local_data_path=local_data_path)
     try:
@@ -44,19 +52,19 @@ async def main():
         candles_df = clob._load_local_dataset("binance_perpetual", "BTCUSDT", "1m")
         if candles_df is None:
             raise FileNotFoundError(
-               f"Candles file BTCUSDT_1m.parquet not found in {local_data_path}."
+                f"Candles file BTCUSDT_1m.parquet not found in {local_data_path}."
             )
-        backtesting._bt_engine.backtesting_data_provider.candles_feeds[
-            "binance_perpetual_BTCUSDT_1m"
-        ] = candles_df
-        backtesting._bt_engine.backtesting_data_provider.start_time = candles_df["timestamp"].min()
-        backtesting._bt_engine.backtesting_data_provider.end_time = candles_df["timestamp"].max()
+        backtesting._bt_engine.backtesting_data_provider.candles_feeds["binance_perpetual_BTCUSDT_1m"] = candles_df
+        #backtesting._bt_engine.backtesting_data_provider.start_time = candles_df["timestamp"].min()
+        #backtesting._bt_engine.backtesting_data_provider.end_time = candles_df["timestamp"].max()
+
+        backtesting._bt_engine.backtesting_data_provider.start_time = start
+        backtesting._bt_engine.backtesting_data_provider.end_time = end
 
         result = await backtesting.run_backtesting(config, start, end, "1m")
         print(result.get_results_summary())
     finally:
         await clob.trades_feeds["binance_perpetual"]._session.close()
-
 
 
 if __name__ == "__main__":
